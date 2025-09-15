aa8cab23b7931046e60baa469ccf05c31da89047
Neha jaiswal <142970307+snnehajaiswal@users.noreply.github.com>
2025-09-15 17:36:09 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useEffect, useState } from 'react'
 import axios from 'axios'
 export const userDataContext=createContext()
 const UserContext = ({children}) => {
-    const serverUrl="http://localhost:8000"
+    const serverUrl="https://virtual-assistant-backend-esea.onrender.com"
     const [userData,setUserData]=useState(null)
      const [frontendImage,setFrontendImage]=useState(null)
      const [backendImage,setBackendImage]=useState(null)
